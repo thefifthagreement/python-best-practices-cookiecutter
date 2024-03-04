edc0ed9a032463ac9ae38d39235aa8fad0402e8e
thefifthagreement <mickael.vitry@gmail.com>
2024-03-04 16:27:18 +0100

    update main

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/main.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/main.py
@@ -1,11 +1,13 @@
 import click
 
-from {{cookiecutter.repo_name}}.{{cookiecutter.repo_name}} import fib
+from bp_test.bp_test import fib
+
 
 @click.command()
-@click.argument('n', type=click.INT)
+@click.argument("n", type=click.INT)
 def cli(n: int):
     print(fib(n))
+
 
 if __name__ == "__main__":
     cli()
